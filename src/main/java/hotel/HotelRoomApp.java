35dd8f943aa9c827fc4e80d895e6cf35a1b7d9e9
nkorojoseph <nkoroahamefula@gmail.com>
2024-12-23 00:24:33 +0100

    e2e stage

--- src/main/java/hotel/HotelRoomApp.java
+++ src/main/java/hotel/HotelRoomApp.java
@@ -4,28 +4,49 @@ import java.awt.EventQueue;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+//import org.apache.logging.log4j.core.tools.picocli.CommandLine.Command;
 
-public class Main {
+import java.util.concurrent.Callable;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
-	private static final Logger LOGGER = LogManager.getLogger(Main.class);
+@Command(mixinStandardHelpOptions = true)
+public class HotelRoomApp implements Callable<Void>{
+	
+	@Option(names = {"--dbName"} , description = "Database name")
+	private String dbName = "";
+	
+	@Option (names = {"--dbHost"} , description = "Database Host Url" )
+	private String dbHost = "";
+	
+	@Option(names = {"--dbPassword" }, description = "Database Password" )
+	private String dbPassword = "";
+	
+	RoomPostgresRepository roomRepository;
+	
+	public static void main (String[] args) {
+		new CommandLine(new HotelRoomApp()).execute(args);
+	}
+	@Override
+	public Void call() throws Exception {
+		// TODO Auto-generated method stub
 
-	public static void main(String[] args) {
-
-		LOGGER.info("App started");
 		EventQueue.invokeLater(() -> {
 		
 			try {
 				
-				RoomPostgresRepository roomRepository;
-				if (args.length > 0 ) {
-					System.err.println("using argument for db");
-					String dbName = args[0];
-					String dbHost = args[1];
-					String dbPassword = args[2];
-					roomRepository = new RoomPostgresRepository(dbHost,dbName, dbPassword);
-				}else {
-					roomRepository = new RoomPostgresRepository();
-				}
+				
+//				if (args.length > 0 ) {
+//					System.err.println("using argument for db");
+//					String dbName = args[0];
+//					String dbHost = args[1];
+//					String dbPassword = args[2];
+//					roomRepository = new RoomPostgresRepository(dbHost,dbName, dbPassword);
+//				}else {
+//					roomRepository = new RoomPostgresRepository();
+//				}
+				roomRepository = new RoomPostgresRepository(dbHost,dbName, dbPassword);
 				HotelRoomView hotelRoomView = new HotelRoomView();
 				RoomController roomController = new RoomController(hotelRoomView, roomRepository);
 				hotelRoomView.setRoomController(roomController);
@@ -37,9 +58,9 @@ public class Main {
 				e.printStackTrace();
 			}
 		});
-
-		LOGGER.info("App terminated");
-
+		return null;
 	}
-
+	
+	
 }
+
